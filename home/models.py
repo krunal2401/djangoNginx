128992fa13b1e2bc924b7d4e5393f079061af872
Krunal <krunalpanchal2401@gmail.com>
2024-01-23 15:30:04 +0530

    make todo app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now=True)
+
+    @staticmethod
+    def get_all_todos():
+        return Todo.objects.all().order_by('-id')
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Todo
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-
+    context = {}
+    todos = Todo.get_all_todos()
+    context['all_todos'] = todos
+    if request.method == 'POST':
+        title=request.POST['title']
+        body=request.POST['body']
+        Todo.objects.create(title = title, body = body)
+        return redirect('home')
+    return render(request, 'index.html', context)
